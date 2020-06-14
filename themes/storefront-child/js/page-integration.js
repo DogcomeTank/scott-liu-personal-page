3f061d2e23711c238772c4e33b8152bb73d0dc9c
Scott Liu <scottliu@MacBook-Pro.hitronhub.home>
2020-06-14 12:33:38 -0700

    Run, Stop, Reset btn function working on dial

--- themes/storefront-child/js/page-integration.js
+++ themes/storefront-child/js/page-integration.js
@@ -1,64 +1,181 @@
 var dialRotate = 0;
 var fillArmRotate = 0;
+var dialAndFillSettings = JSON.parse(dialFillSettings.settings);
+var setTime = 0;
+var setTimeInterval = 200;
+var stopBtn = false;
+jQuery(document).ready(function () {
 
 
-jQuery("#dial-ccw-slow-btn").click(function(){
+    jQuery('#run-btn').click(function () {
+        autoRun(dialAndFillSettings);
+    });
+    jQuery('#stop-btn').click(function () {
+        stopBtn = true;
+    });
+    jQuery('#reset-btn').click(function () {
+        resetBtnPressed();
+    });
+
+});
+
+
+
+// **********
+
+async function ResetDelayed(){
+    dialRotate += .2;
+    if(dialRotate < 0){
+        rotate('dial', dialRotate);
+        await delay();
+    }
+}
+async function resetBtnPressed(){
+    while(dialRotate < 0){
+        await ResetDelayed();
+    }
+}
+
+async function AutoRunDelayed() {
+    // notice that we can await a function
+    // that returns a promise
+    dialRotate -= .2;
+    rotate('dial', dialRotate);
+    await delay();
+}
+async function autoRun(array) {
+
+    jQuery('#run-btn').prop('disabled', true);
+
+    if(jQuery( "#run-btn" ).hasClass( "w3-green" )){
+        jQuery( "#run-btn" ).removeClass( "w3-green" ).addClass('w3-grey');
+    }
+
+    for (const item of array) {
+        if(!stopBtn){
+            await AutoRunDelayed(item);
+        }else{
+
+            // if stop button pressed, stop the operation
+            stopBtn = false;
+            break;
+        }
+    }
+    
+    jQuery('#run-btn').prop('disabled', false);
+    if(jQuery( "#run-btn" ).hasClass( "w3-grey" )){
+        jQuery( "#run-btn" ).removeClass( "w3-grey" ).addClass('w3-green');
+    }
+}
+
+async function resetBtn(currentPosition) {
+
+    jQuery('#run-btn').prop('disabled', true);
+
+    if(jQuery( "#run-btn" ).hasClass( "w3-green" )){
+        jQuery( "#run-btn" ).removeClass( "w3-green" ).addClass('w3-grey');
+    }
+
+    for (const item of array) {
+        if(!stopBtn){
+            await AutoRunDelayed(item);
+        }else{
+            // if stop button pressed, stop the operation
+            stopBtn = false;
+            break;
+        }
+    }
+    
+    jQuery('#run-btn').prop('disabled', false);
+    if(jQuery( "#run-btn" ).hasClass( "w3-grey" )){
+        jQuery( "#run-btn" ).removeClass( "w3-grey" ).addClass('w3-green');
+    }
+
+}
+
+function delay() {
+    return new Promise(resolve => setTimeout(resolve, setTimeInterval));
+}
+//******* */
+
+
+
+
+// Btn Events
+jQuery("#dial-ccw-slow-btn").click(function () {
     dialRotate -= .2;
     rotate("dial", dialRotate);
 });
 
-jQuery("#fill-ccw-slow-btn").click(function(){
+jQuery("#fill-ccw-slow-btn").click(function () {
     fillArmRotate -= .2;
     rotate("fill-arm", fillArmRotate);
 });
 
-jQuery("#dial-ccw-fast-btn").click(function(){
+jQuery("#dial-ccw-fast-btn").click(function () {
     dialRotate -= 1;
     rotate("dial", dialRotate);
 });
 
-jQuery("#fill-ccw-fast-btn").click(function(){
+jQuery("#fill-ccw-fast-btn").click(function () {
     fillArmRotate -= 1;
     rotate("fill-arm", fillArmRotate);
 });
 
 
 // CW btn
-jQuery("#dial-cw-slow-btn").click(function(){
+jQuery("#dial-cw-slow-btn").click(function () {
     dialRotate += .2;
     rotate("dial", dialRotate);
 });
 
-jQuery("#fill-cw-slow-btn").click(function(){
+jQuery("#fill-cw-slow-btn").click(function () {
     fillArmRotate += .2;
     rotate("fill-arm", fillArmRotate);
 });
 
-jQuery("#dial-cw-fast-btn").click(function(){
+jQuery("#dial-cw-fast-btn").click(function () {
     dialRotate += 1;
     rotate("dial", dialRotate);
 });
 
-jQuery("#fill-cw-fast-btn").click(function(){
+jQuery("#fill-cw-fast-btn").click(function () {
     fillArmRotate += 1;
     rotate("fill-arm", fillArmRotate);
 });
 
-
-
-function rotate(imageName, rotation){
+function rotate(imageName, rotation) {
 
     dialRotate.toFixed(4);
     fillArmRotate.toFixed(4);
 
-    var img=document.getElementById(imageName);
+    var img = document.getElementById(imageName);
 
-    img.setAttribute('style','transform:rotate('+rotation+'deg)');
+    img.setAttribute('style', 'transform:rotate(' + rotation + 'deg)');
 
     // display the rotation angle
-    if(imageName == 'dial'){
+    if (imageName == 'dial') {
         jQuery('#dial-rotation').html(dialRotate.toFixed(4));
-    }else if(imageName == 'fill-arm'){
+    } else if (imageName == 'fill-arm') {
+        jQuery('#fill-arm-rotation').html(fillArmRotate.toFixed(4));
+    }
+
+}
+
+
+async function AutoRotate(imageName, rotation) {
+
+    dialRotate.toFixed(4);
+    fillArmRotate.toFixed(4);
+
+    var img = document.getElementById(imageName);
+
+    img.setAttribute('style', 'transform:rotate(' + rotation + 'deg)');
+
+    // display the rotation angle
+    if (imageName == 'dial') {
+        jQuery('#dial-rotation').html(dialRotate.toFixed(4));
+    } else if (imageName == 'fill-arm') {
         jQuery('#fill-arm-rotation').html(fillArmRotate.toFixed(4));
     }
 
--- themes/storefront-child/page_integration.php
+++ themes/storefront-child/page_integration.php
@@ -6,6 +6,15 @@
 function enqueue_page_integration() {
     wp_enqueue_style ( 'integration-style', get_stylesheet_directory_uri() . '/css/page-integration.css' );
     wp_enqueue_script ( 'integration-script', get_stylesheet_directory_uri() . '/js/page-integration.js' ,  array(), time(),  'true' );
+
+    // get the setting for dial and fill arm
+    global $wpdb;
+    $sql_dial_fill_setting = "SELECT * FROM sl_dial_fill_setting";
+    
+    $settings = $wpdb->get_results ($sql_dial_fill_setting);
+    $settings_json = json_encode($settings);
+    $nonce = wp_create_nonce("my_user_play_nonce");
+    wp_localize_script( 'integration-script', 'dialFillSettings', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ),'nonce'=>$nonce, 'settings'=>$settings_json));
 }
 
 add_action( 'wp_enqueue_scripts', 'enqueue_page_integration' );
@@ -15,44 +24,56 @@ get_header();
 
 
 <div class="w3-container">
+    <p>
+        <button id="run-btn" class="w3-button w3-green w3-round">Run</button>
+        <button id="stop-btn" class="w3-button w3-red w3-round">Stop</button>
+        <button id="reset-btn" class="w3-button w3-yellow w3-round">Reset</button>
+    </p>
     <div class="machine">
-        <img src="https://scott-liu.ca/wp-content/uploads/2020/06/DialArtboard-1-copy-2.svg" class="dial" id="dial"
-            onclick="rotate();">
+        <img src="https://scott-liu.ca/wp-content/uploads/2020/06/DialArtboard-1-copy-2.svg" class="dial" id="dial">
         <img src="https://scott-liu.ca/wp-content/uploads/2020/06/FillArtboard-1-copy-2.svg" class="fill-arm"
-            id="fill-arm" onclick="rotate();">
+            id="fill-arm">
     </div>
 
     <div>
-    <p>Dial Rotation: <span id="dial-rotation">0.0000</span></p>
-    <p>Fill Arm Rotation: <span id="fill-arm-rotation">0.0000</span></p>
+        <p>Dial Rotation: <span id="dial-rotation">0.0000</span></p>
+        <p>Fill Arm Rotation: <span id="fill-arm-rotation">0.0000</span></p>
     </div>
 
     <div class="w3-row">
         <div class="w3-col s3">
             <h5>Slow</h5>
-            <p><button id="dial-ccw-slow-btn" class="w3-button w3-black w3-round"><i class="fas fa-undo"></i> Dial</button></p>
-            <p><button id="fill-ccw-slow-btn" class="w3-button w3-black w3-round"><i class="fas fa-undo"></i> Fill Arm</button></p>
+            <p><button id="dial-ccw-slow-btn" class="w3-button w3-black w3-round"><i class="fas fa-undo"></i>
+                    Dial</button></p>
+            <p><button id="fill-ccw-slow-btn" class="w3-button w3-black w3-round"><i class="fas fa-undo"></i> Fill
+                    Arm</button></p>
         </div>
         <div class="w3-col s3">
-        <h5>Fast</h5>
-            <p><button id="dial-ccw-fast-btn" class="w3-button w3-black w3-round"><i class="fas fa-undo"></i> Dial</button></p>
-            <p><button id="fill-ccw-fast-btn" class="w3-button w3-black w3-round"><i class="fas fa-undo"></i> Fill Arm</button></p>
+            <h5>Fast</h5>
+            <p><button id="dial-ccw-fast-btn" class="w3-button w3-black w3-round"><i class="fas fa-undo"></i>
+                    Dial</button></p>
+            <p><button id="fill-ccw-fast-btn" class="w3-button w3-black w3-round"><i class="fas fa-undo"></i> Fill
+                    Arm</button></p>
         </div>
         <div class="w3-col s3">
             <h5>Slow</h5>
-            <p><button id="dial-cw-slow-btn" class="w3-button w3-black w3-round"><i class="fas fa-redo"></i> Dial</button></p>
-            <p><button id="fill-cw-slow-btn" class="w3-button w3-black w3-round"><i class="fas fa-redo"></i> Fill Arm</button></p>
+            <p><button id="dial-cw-slow-btn" class="w3-button w3-black w3-round"><i class="fas fa-redo"></i>
+                    Dial</button></p>
+            <p><button id="fill-cw-slow-btn" class="w3-button w3-black w3-round"><i class="fas fa-redo"></i> Fill
+                    Arm</button></p>
         </div>
         <div class="w3-col s3">
-        <h5>Fast</h5>
-            <p><button id="dial-cw-fast-btn" class="w3-button w3-black w3-round"><i class="fas fa-redo"></i> Dial</button></p>
-            <p><button id="fill-cw-fast-btn" class="w3-button w3-black w3-round"><i class="fas fa-redo"></i> Fill Arm</button></p>
+            <h5>Fast</h5>
+            <p><button id="dial-cw-fast-btn" class="w3-button w3-black w3-round"><i class="fas fa-redo"></i>
+                    Dial</button></p>
+            <p><button id="fill-cw-fast-btn" class="w3-button w3-black w3-round"><i class="fas fa-redo"></i> Fill
+                    Arm</button></p>
         </div>
     </div>
 
 
-    
-    
+
+
 </div>
 
 
